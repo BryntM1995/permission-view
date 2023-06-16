704a950b29cc42bf4522eeb3c65fb7fd740c9649
Bryant <bryant-m@hotmail.com>
2023-06-15 21:24:58 -0400

    All basic features added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,318 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <section>
+      <h4>Permission List</h4>
+      <h4 v-show="allData.length === 0" class="mt-2">There is not data to display</h4>
+      <!-- Button trigger modal -->
+      <button @click="setEmptyValues" type="button" class="btn btn-primary m-1" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Request a Permission
+      </button>
+      <!-- Button trigger modal -->
+      <button @click="setEmptyValues" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Add Permission description
+      </button>
+    </section>
+    <section class="row row-cols-3">
+      <div v-for="(entity, index) in allData" :key="index" class="col">
+        <div class="card cardMainDiv">
+          <img src="../assets/logo.png" class="card-img-top" alt="nothing">
+          <div class="card-body">
+            <h5 class="card-title">{{ entity.employeeFirstName }} {{ entity.employeeLastName }} </h5>
+            <h5 id="permissionName">{{ dataConverter(entity.permissionTypeId) }}</h5>
+            <p class="card-text">Date: {{ entity.permissionDate | formatDate('MMMM Do YYYY, h:mm:ss a') }}</p>
+            <button @click="requestDeletion(entity.id)" class="btn btn-primary me-2">Delete</button>
+            <button @click="drawAModal(index, entity.id)" type="button" class="btn btn-primary" data-bs-toggle="modal"
+              data-bs-target="#editModal">Edit</button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section>
+      <!-- Modal -->
+      <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header mb-3">
+              <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="input-group mb-3">
+                <span class="input-group-text">First Name</span>
+                <input v-model="employeeFirstName" type="text" aria-label="First name" class="form-control">
+              </div>
+              <div class="input-group mb-3">
+                <span class="input-group-text">Last Name</span>
+                <input v-model="employeeLastName" type="text" aria-label="Last name" class="form-control">
+              </div>
+              <div class="mb-3">
+                <select v-model="permissionTypeId" class="form-select" aria-label="Default select example">
+                  <option value="" selected>Open this select menu</option>
+                  <option v-for="(entityType, index) in allDataType" :key="index" :value="entityType.id">{{
+                    entityType.description }}</option>
+                </select>
+              </div>
+              <div class="input-group mb-3">
+                <span class="input-group-text">Requested Date</span>
+                <input v-model="permissionDate" type="datetime-local" aria-label="First name" class="form-control">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button @click="setEmptyValues" type="button" class="btn btn-secondary"
+                data-bs-dismiss="modal">Close</button>
+              <button @click="toConfirm" type="button" data-bs-dismiss="modal" class="btn btn-primary">Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal2 -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="input-group mb-3">
+                <span class="input-group-text">Please name this description</span>
+                <input v-model="description" type="text" aria-label="First name" class="form-control"
+                  placeholder="Let us know the reason why you are missing.">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button @click="addPermissionType" type="button" class="btn btn-primary" data-bs-dismiss="modal">Save
+                changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Modal3 -->
+      <div class="modal fade" id="editModal" data-bs-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Edit a Permission</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <div class="input-group mb-3">
+                <span class="input-group-text">First Name</span>
+                <input v-model="employeeFirstName" type="text" aria-label="First name" class="form-control">
+              </div>
+              <div class="input-group mb-3">
+                <span class="input-group-text">Last Name</span>
+                <input v-model="employeeLastName" type="text" aria-label="Last name" class="form-control">
+              </div>
+              <div class="mb-3">
+                <select v-model="permissionTypeId" class="form-select" aria-label="Default select example">
+                  <option value="" selected>Open this select menu</option>
+                  <option v-for="(entityType, index) in allDataType" :key="index" :value="entityType.id">{{
+                    entityType.description }}</option>
+                </select>
+              </div>
+              <div class="input-group mb-3">
+                <span class="input-group-text">Requested Date</span>
+                <input v-model="permissionDate" type="datetime-local" aria-label="First name" class="form-control">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button @click="setEmptyValues" type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button @click="editPermission" data-bs-dismiss="modal" type="button" class="btn btn-primary">Save
+                changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
+
 <script>
+import moment from 'moment';
+import Swal from 'sweetalert2';
+import { mapState, mapActions } from 'vuex';
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
-  }
+  },
+  filters: {
+    formatDate(value, format) {
+      if (!value) return '';
+      return moment(value).format(format);
+    },
+
+
+  },
+  computed: {
+    ...mapState(['allData']),
+    ...mapState(['allDataType']),
+  },
+  data() {
+    return {
+      id: "",
+      employeeFirstName: "",
+      employeeLastName: "",
+      permissionDate: "",
+      description: "",
+      permissionTypeId: null,
+    }
+  },
+  methods: {
+    ...mapActions(['fetchList']),
+    ...mapActions(['sendData']),
+    ...mapActions(['fetchListType']),
+    ...mapActions(['sendDataType']),
+    ...mapActions(['deletePermission']),
+    ...mapActions(['deletePermissionType']),
+    ...mapActions(['updatePermission']),
+    ...mapActions(['updatePermissionType']),
+    async  toSendPermissionData() {
+      const permissionData = {
+        employeeFirstName: this.employeeFirstName,
+        employeeLastName: this.employeeLastName,
+        permissionTypeId: this.permissionTypeId,
+        permissionDate: this.permissionDate,
+      };
+
+      await this.sendData(permissionData);
+      this.fetchList();
+      this.setEmptyValues();
+    },
+    async addPermissionType() {
+      const dataType = {
+        description: this.description,
+      }
+      await this.sendDataType(dataType);
+      this.fetchList();
+      this.setEmptyValues();
+    },
+    setEmptyValues() {
+      this.employeeFirstName = '',
+        this.employeeLastName = '',
+        this.permissionDate = '',
+        this.description = ''
+      this.permissionTypeId = null;
+    },
+    toConfirm() {
+      Swal.fire({
+        title: 'Confirmation',
+        text: 'Are you sure you want to perform this action?',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'Cancel',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.toSendPermissionData();
+          Swal.fire('Action Performed', 'The action has been successfully completed.', 'success');
+        } else {
+          this.setEmptyValues();
+          Swal.fire('Action Canceled', 'The action has been canceled.', 'info');
+        }
+      });
+    },
+    dataConverter(id) {
+      if (!id) return "No Available Reason"
+      const permissionType = this.allDataType.find(x => x.id == id);
+      return permissionType.description;
+    },
+     requestDeletion(id) {
+      Swal.fire({
+        title: 'Do you want to save the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+      }).then( async (result) => {
+        if (result.isConfirmed) {
+          await this.deletePermission(id);
+          Swal.fire('Saved!', '', 'success')
+          this.fetchList();
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+    },
+    async deletePermissionType(id) {
+      Swal.fire({
+        title: 'Do you want to save the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+      }).then( async (result) => {
+        if (result.isConfirmed) {
+          await this.deletePermissionType(id);
+          Swal.fire('Saved!', '', 'success')
+          
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+    },
+    async editPermission() {
+      
+      const newValue = {
+        id: this.id,
+        employeeFirstName: this.employeeFirstName,
+        employeeLastName: this.employeeLastName,
+        permissionTypeId: this.permissionTypeId,
+        permissionDate: this.permissionDate
+      }
+      await this.updatePermission(newValue)
+      this.fetchList();
+      this.setEmptyValues();
+    },
+    drawAModal(id , entityId) {
+      this.id = entityId
+      const newPermission = this.allData[id];
+      this.employeeFirstName = newPermission.employeeFirstName,
+        this.employeeLastName = newPermission.employeeLastName,
+        this.permissionDate = newPermission.permissionDate,
+        this.permissionTypeId = newPermission.permissionTypeId
+    },
+  },
+  created() {
+    this.fetchList();
+    this.fetchListType();
+  },
 }
+//vee validate, remove, update
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+img {
+  border-radius: 50%;
+  height: 80px;
+  width: 35%;
+  border: solid black;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.cardMainDiv {
+  display: flex;
+  place-items: center;
+  text-align: center;
+  margin-top: 1%;
+  margin-bottom: 2%;
+  padding-top: 2%;
+  background: linear-gradient(45deg, yellowgreen, transparent);
+  width: 50%
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+h4 {
+  font-weight: 500;
+  border: solid 2px;
+  text-align: center;
+  background: linear-gradient(45deg, yellowgreen, transparent);
 }
-a {
-  color: #42b983;
+
+#permissionName {
+  text-transform: capitalize;
+  color: gray;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,86 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    allData: [],
+    allDataType: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    setAllData(state, allData) {
+      state.allData = allData;
+    },
+    setAllDataType(state, allDataType) {
+      state.allDataType = allDataType;
+    },
   },
   actions: {
+    async fetchList({ commit }) {
+      const response = await axios.get(
+        "https://localhost:44377/api/Permission"
+      );
+      commit("setAllData", response.data);
+    },
+    async fetchListType({ commit }) {
+      const response = await axios.get(
+        "https://localhost:44377/api/PermissionType"
+      );
+      commit("setAllDataType", response.data);
+    },
+    async sendData({ commit }, data) {
+      const response = await axios.post(
+        "https://localhost:44377/api/Permission",
+        data
+      );
+      commit("addData", response.data);
+    },
+    async sendDataType({ commit }, data) {
+      try {
+        const response = await axios.post(
+          "https://localhost:44377/api/PermissionType",
+          data
+        );
+        commit("addDataType", response.data);
+        alert("Sent!");
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deletePermission({ commit }, id) {
+      const response = await axios.delete(
+        `https://localhost:44377/api/Permission/${id}`
+      );
+      commit("deleteItem", id);
+    },
+    async deletePermissionType({ commit }, id) {
+      const response = await axios.delete(
+        `https://localhost:44377/api/PermissionType/${id}`
+      );
+      commit("deleteItem", id);
+    },
+    async updatePermission({ commit }, newData) {
+      await axios.put(
+        `https://localhost:44377/api/Permission/${newData.id}`,
+        newData
+      );
+      commit('updateItem',newData)
+    },
+    async updatePermissionType({ commit }, id, newData) {
+      axios
+        .put(`https://localhost:44377/api/PermissionType/${id}`, {
+          value: newData,
+        })
+        .then((response) => {
+          commit("updateItem", response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
